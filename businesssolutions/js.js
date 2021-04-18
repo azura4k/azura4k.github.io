67ccb410d6c501a97543b42dfe8c396dd1f5dff0
azura4k <47215731+azura4k@users.noreply.github.com>
2021-04-17 21:12:31 -0400

    Complete Redesign and Merge over by hand

---
+++ businesssolutions/js.js
@@ -0,0 +1,22 @@
+const links = document.querySelectorAll(".btnlink");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+var submitted = false;
+$('#gform').on('submit', function (e) {
+  $('#gform *').fadeOut(100);
+
+  $('#gform').prepend('<h2>Thank You</h2><br><p>We will be in touch, soon.</p>');
+});
